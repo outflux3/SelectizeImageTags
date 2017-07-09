ab5c5cadea560516173df2374b6ef8b9a0ecba9b
Toutouwai <rpsallis@gmail.com>
2017-07-09 13:44:12 +1200

    Fixes for AJAX-loaded fields
    Hooks "renderReadyHook" instead of "render" so that JS config is
    populated for AJAX-loaded fields. As a consequence, minimum required PW
    version is now 3.0.61.
    Moves JS initialisation to a function. Function is called when image
    field is 'reloaded' (fix for AJAX-loaded fields) and when images are
    uploaded (fix for new uploads) as well as DOM ready.
    JS checks for modified inputfield name when rendered inside a repeater
    item.
    Also fix for newline character in tags config field.

--- SelectizeImageTags.js
+++ SelectizeImageTags.js
@@ -3,21 +3,27 @@
  * ============================================
  */
 
-function lookup(array, prop, value) {
-    for (var i = 0, len = array.length; i < len; i++)
-        if (array[i] && array[i][prop] === value) return array[i];
-}
-
-$(document).ready(function () {
+function initSelectizeImageTags() {
 
     for(var name in config.SelectizeImageTags) {
+
+        // get inputfields
+        var thisTagsFields = $("." + name);
+        // get inputfields inside repeaters
+        var thisRepeaterTagsFields = $("li[class*='" + name + "_repeater']").filter(function() {
+            var re = new RegExp("\\b" + name + "_repeater");
+            return this.className.match(re);
+        });
+        // get the inputs inside those inputfields
+        var thisTagsInputs = thisTagsFields.add(thisRepeaterTagsFields).find("input[name^='tags_']");
+
         // at this point, name is our fieldName, and field holds the selectize init object
         var field = config.SelectizeImageTags[name];
 
         if(field.create == true) {
+
             // each image has it's own tags field...
-            thisTagsFields = $("." + name + " input[name^='tags_']");
-            thisTagsFields.each(function() {
+            thisTagsInputs.each(function() {
 
                 var thisTagsField = $(this);
                 var thisTags = thisTagsField.val();
@@ -34,10 +40,27 @@ $(document).ready(function () {
 
         } // end if value.create
 
-        $("." + name + " input[name^='tags_']").selectize(field);
+        thisTagsInputs.selectize(field);
 
     }
+}
 
+function lookup(array, prop, value) {
+    for (var i = 0, len = array.length; i < len; i++)
+        if (array[i] && array[i][prop] === value) return array[i];
+}
+
+// Init on DOM ready
+$(document).ready(function () {
+    initSelectizeImageTags();
 });
 
+// Init on AJAX-loading of image field
+$(document).on('reloaded', '.InputfieldImage', function() {
+    initSelectizeImageTags();
+});
 
+// Init on image uploaded
+$(document).on('AjaxUploadDone', '.gridImages', function() {
+    initSelectizeImageTags();
+});
