c729ac2a9b7f1924157ede56e90738b3a2ad714f
Marc <mw@ajcg.org>
2017-08-09 13:31:48 -0400

    Init field from module, not core
    Allows the module to init the image fields properly

---
+++ SelectizeImageTags.info.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$info = array(
+    'title' => 'Selectize Image Tags',
+    'author' => 'Macrura',
+    'version' => 005,
+    'summary' => 'Admin helper for enabling selectize.js on images tags field.',
+    'href' => '',
+    'icon' => 'tags',
+    'singular' => true,
+    'autoload' => 'template=admin',
+    'requires'  => array(
+        'ProcessWire>=3.0.61'
+    )
+);
+
+if($this->wire('config')->version < '3.0.67') {
+	$info['requires'][] = "JquerySelectize";
+}
