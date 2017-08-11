d4160b20f40e05840b6cb5d947fb63b3a95aeb4c
Marc <mw@ajcg.org>
2017-08-11 12:15:29 -0400

    sommin

--- SelectizeImageTags.info.php
+++ SelectizeImageTags.info.php
@@ -15,10 +15,6 @@ $info = array(
     )
 );
 
-if($this->wire('config')->version < '3.0.67') {
+if($this->wire('config') && $this->wire('config')->version < '3.0.67') {
 	$info['requires'][] = "JquerySelectize";
-}
-
-if($this->wire('modules')->get("TracyDebugger")) {
-    bd($info);
 }
